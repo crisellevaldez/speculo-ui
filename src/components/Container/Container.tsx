55f6d1e28826e4b9ea0fd77bef2233a0034f3526
= <crisellevaldez20@gmail.com>
2024-12-17 10:12:08 +0800

    fix: reorder Container breakpoint classes for proper specificity

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -23,7 +23,9 @@ export const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
       <Component
         ref={ref}
         className={cn(
-          "3xl:w-[2000px] w-full 2xl:w-[1500px]",
+          "w-full",
+          "2xl:w-[1500px]",
+          "3xl:w-[2000px]",
           padding && "px-4 sm:px-6 lg:px-8",
           centered && "mx-auto",
           className,
